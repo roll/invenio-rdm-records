dd2dbce2e89353c18999e34e32de994f646771c6
roll <eskarev@gmail.com>
2024-04-23 14:10:01 +0100

    Mapped based data package properties

--- invenio_rdm_records/resources/serializers/datapackage/schema.py
+++ invenio_rdm_records/resources/serializers/datapackage/schema.py
@@ -7,12 +7,19 @@
 
 """Data Package based Schema for Invenio RDM Records."""
 
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, missing
+
+PROFILE_URL = "https://datapackage.org/profiles/2.0/datapackage.json"
 
 
 class DataPackageSchema(Schema):
     """Schema for Data Package in JSON."""
 
-    profile = fields.Constant(
-        "https://datapackage.org/profiles/2.0/datapackage.json", data_key="$schema"
-    )
+    profile = fields.Constant(PROFILE_URL, data_key="$schema")
+
+    id = fields.Str(attribute="links.doi")
+    title = fields.Str(attribute="metadata.title")
+    description = fields.Str(attribute="metadata.description")
+    version = fields.Str(attribute="metadata.version")
+    created = fields.Str(attribute="created")
+    homepage = fields.Str(attribute="links.self_html")
--- tests/resources/serializers/test_datapackage_serializer.py
+++ tests/resources/serializers/test_datapackage_serializer.py
@@ -13,10 +13,15 @@ from invenio_rdm_records.resources.serializers.datapackage import DataPackageSer
 from invenio_rdm_records.resources.serializers.errors import VocabularyItemNotFoundError
 
 
-def test_data_package_serializer_minimal_record(minimal_record_to_dict):
+def test_data_package_serializer_minimal_record(full_record_to_dict):
     serializer = DataPackageSerializer()
-    serialized_record = serializer.dump_obj(minimal_record_to_dict)
-
+    serialized_record = serializer.dump_obj(full_record_to_dict)
     assert serialized_record == {
-        "$schema": "https://datapackage.org/profiles/2.0/datapackage.json"
+        "$schema": "https://datapackage.org/profiles/2.0/datapackage.json",
+        "id": "https://handle.stage.datacite.org/10.1234/inveniordm.1234",
+        "title": "InvenioRDM",
+        "description": "<h1>A description</h1> <p>with HTML tags</p>",
+        "version": "v1.0",
+        "created": "2023-11-14T18:30:55.738898+00:00",
+        "homepage": "https://127.0.0.1:5000/records/12345-abcde",
     }
